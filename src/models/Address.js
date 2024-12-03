c7d35e0aea37c80016125a601c9a599243ba6453
Truong-Viet-Vu <trvietvu2005@gmail.com>
2024-12-03 22:56:28 +0700

    fix address

--- src/models/Address.js
+++ src/models/Address.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 
 const addressSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
     address: { type: String, required: true },
     city: { type: String, required: true },
     postalCode: { type: String, required: true },
